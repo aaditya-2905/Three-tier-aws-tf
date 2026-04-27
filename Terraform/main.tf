9b277f713c45334266fd01e5516436c7747dfc09
Aadityasinh zala <zalaaadityasinh9@gmail.com>
2026-04-27 18:52:37 +0530

    Fixed arn splitting error

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -80,6 +80,7 @@ module "sg" {
       description = "Security group for secondary region"
       vpc_id      = module.vpc.vpc_ids["secondary"]
       environment = "prod"
+      environment = "prod"
 
       ingress_rules = [
         { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
